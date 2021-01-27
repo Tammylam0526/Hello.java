392f59bd23e6ceb3fa922581966157ee998b3101
Tammylam0526 <78098950+Tammylam0526@users.noreply.github.com>
2021-01-27 12:02:21 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Tammy Lam");
+		System.out.println("Lab 01");
+		System.out.println("Insturctions PC");
+		System.out.println("1/27/2021");
+		System.out.println("CMSC255");
+		System.out.println();
+		
+	}
+}
+
+	
+
+	
+		
