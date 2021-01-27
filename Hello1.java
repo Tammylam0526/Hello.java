3b96705c2623193b445493f7c815bf1d51d6610c
Tammylam0526 <78098950+Tammylam0526@users.noreply.github.com>
2021-01-27 12:20:15 -0500

    Add files via upload
    updated version

---
+++ Hello1.java
@@ -0,0 +1,23 @@
+/************************************************
+* Lab 01 - Instructions PC-1.pdf
+*************************************************
+*Tammy Lam
+*1/27/2021
+*CMSC 255
+**************************************************
+
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Tammy Lam");
+		System.out.println("Lab 01");
+		System.out.println("Intructions for PC");
+		System.out.println("1/27/2021");
+		System.out.println("CMSC255");
+		System.out.println();	
+	}
+}
+
+	
+
+	
+		
